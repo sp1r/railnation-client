7146f3fb6ede460032ca0c67a5f8773c94b42610
Vyacheslav Spiridonov <vspiridonov@bcc.ru>
2017-01-19 12:44:54 +0300

    switch to personal logger

--- lib/railnation/core/common.py
+++ lib/railnation/core/common.py
@@ -6,6 +6,7 @@ Not configurable.
 """
 
 import os
+import logging
 
 # Try to guess where we are
 base_dir = os.path.normpath(os.path.join(
@@ -19,4 +20,8 @@ html_dir = os.path.join(base_dir, 'html')
 lib_dir = os.path.join(base_dir, 'lib')
 
 
-
+log = logging.getLogger('RailNation')
+log_handler = logging.StreamHandler()
+log_handler.setFormatter(logging.Formatter(fmt='%(asctime)s: %(levelname)8s: %(name)s: %(message)s',
+                                           datefmt='%Y-%d-%m %H:%M:%S'))
+log.addHandler(log_handler)
