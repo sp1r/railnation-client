bebb9f16864ac7412fe7cd9cbabdb66b19335801
Vopros08 <Vopros92@gmail.com>
2017-01-20 10:20:43 +0300

    login ajax

--- html/js/custom.js
+++ html/js/custom.js
@@ -2,21 +2,51 @@ $(document).on('ready',function(){
 
     $('.login-form').on('submit', function(e){
         e.preventDefault();
-
-        console.log($(e.target).serialize());
-        return;
+        var username = $('input[name=username]').val(),
+            password = $('input[name=password]').val();
 
         $.ajax({
-            url: "",
-            type: "POST",
-            data: this.serialize(),
-            success: function (d) {
-                console.log('success', d);
-            },
-            error: function (d) {
-                console.log('error', d);
+            url: 'api/v1/login',
+            type: 'POST',
+            contentType: "application/json",
+            data: JSON.stringify({
+                "username": username,
+                "password": password
+            }),
+            success: function (data) {
+                console.log(data);
+
+                if(data.code === 0){
+                    $.ajax({
+                        url: 'api/v1/worlds',
+                        type: 'GET',
+                        contentType: "application/json",
+                        success: function (d) {
+                            if(d.code === 0){
+                                if(d.data.length){
+                                    var worlds = d.data;
+
+
+                                    console.log(worlds);
+
+                                }
+                            }
+                        },
+                        error: function (data) {
+                            console.log(data);
+                        }
+                    });
+                }else{
+                    console.log('error');
+                }
+
+
             }
         });
+
+
+
+
     });
 
 });
