8943358efeebe00fa992a322e7ab618a56821200
Vyacheslav Spiridonov <vspiridonov@bcc.ru>
2017-01-30 15:34:34 +0300

    trains manager skel

---
+++ lib/railnation/managers/trains.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding:  utf-8 -*-
+
+import cherrypy
+from cherrypy.process.plugins import Monitor
+
+from railnation.core.common import log
+from railnation.core.server import server
+from railnation.core.errors import RailNationClientError
+
+
+class TrainsManager:
+    """
+    Trains manager.
+    """
+
+    instance = None
+
+    @staticmethod
+    def get_instance():
+        """
+        :rtype: TrainsManager
+        """
+        if TrainsManager.instance is None:
+            TrainsManager.instance = TrainsManager()
+
+        return TrainsManager.instance
+
+    def __init__(self):
+        self.log = log.getChild('TrainsManager')
+        self.log.debug('Initializing...')
+
+    def refresh(self):
+        pass
+
+
+Monitor(cherrypy.engine, TrainsManager.get_instance().refresh, frequency=10).subscribe()
+
